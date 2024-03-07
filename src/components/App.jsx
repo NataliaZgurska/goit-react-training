7b1f5cb7ba92b3d76788e9b4df7fdccb2b011a61
NataliaZgurska <143435353+NataliaZgurska@users.noreply.github.com>
2024-03-07 10:50:21 +0200

    update

---
+++ src/components/App.jsx
@@ -0,0 +1,37 @@
+// src/components/App.jsx
+const favouriteBooks = [
+  { id: "id-1", name: "JS for beginners" },
+  { id: "id-2", name: "React basics" },
+  { id: "id-3", name: "React Router overview" }
+];
+
+
+import Product from "./product";
+import BookList from "./BookList";
+
+
+export default function App() {
+  return (
+  <>
+    <div>
+      <h1>Best selling</h1>
+
+      <Product
+        name="Tacos With Lime"
+        imgUrl="https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640"
+        price={10.99}
+      />
+      <Product
+        name="Fries and Burger"
+        imgUrl="https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?dpr=2&h=480&w=640"
+        price={14.29}
+      />
+    </div>
+
+    <div>
+			<h1>Books of the week</h1>
+      <BookList books={favouriteBooks} />
+    </div>
+  </>
+  );
+}
