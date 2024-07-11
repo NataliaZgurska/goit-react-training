984e562bc54cd45bb52d2755a83caa2424e5e6e9
NataliaZgurska <143435353+NataliaZgurska@users.noreply.github.com>
2024-07-11 13:31:16 +0300

    react-hook-form

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,60 +1,131 @@
-// src/components/App.jsx
-import { useState } from 'react';
-const favouriteBooks = [
-  { id: "id-1", name: "JS for beginners" },
-  { id: "id-2", name: "React basics" },
-  { id: "id-3", name: "React Router overview" }
-];
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
 
-import productList from './productList.json'
-import cats from './cats.json'
-
-import BestProduct from "./BestProduct/BestProduct";
-import BookList from "./BookList";
-import Cats from './Cats/Cats';
-import CatsListBtn from './CatsListBtn/CatsListBtn';
-
-
-const initialLikes = { cat1: 0, cat2: 0, cat3: 0, cat4: 0 };
-
+const schema = yup
+  .object({
+    name: yup.string().required('Name is required'),
+    gender: yup
+      .string()
+      .oneOf(['female', 'male'], 'Gender is required')
+      .required('Gender is required'),
+    email: yup.string().email(),
+    weight: yup.number().positive().required('Weight is required'),
+    sportTime: yup.number().positive().required('sportTime is required'),
+    waterAmount: yup.number().positive().required('sportTime is required'),
+  })
+  .required();
 
 export default function App() {
-  const [likes, setLikes] = useState(initialLikes );
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+    defaultValues: {
+      gender: 'female',
+    },
+  });
+  const onSubmit = data => console.log(data);
 
-  const handleLogLikes = (catName) => {
-  setLikes({ ...likes, [catName]: likes[catName] + 1 })
-  };
-  const handleResetLikes = () => { setLikes(initialLikes); };
- const totalLike = Object.values(likes).reduce((acc, item) => acc+item, 0)
- 
-  
   return (
-    <>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <div>
-        <h1>Send Likes for cats</h1>
-        <CatsListBtn likes={likes}
-          handleLogLikes={handleLogLikes}
-          handleResetLikes={handleResetLikes}
-       totalLike={totalLike}
-        />
-      </div>
-      <div>
-        <h1>Best cats</h1>
-        <Cats cats={cats} />
+        <label>Your gender identity</label>
+        <input type="radio" value="female" {...register('gender')} /> Woman
+        <input type="radio" value="male" {...register('gender')} /> Man
+        <p>{errors.gender?.message}</p>
       </div>
 
-     <div>
-			<h1>Books of the week you have to read</h1>
-      <BookList books={favouriteBooks} />
-      </div>
-      
       <div>
-        <h1>Best products</h1>
-        <BestProduct productList={ productList} />
+        <label>Your name</label>
+        <input {...register('name')} />
+        <p>{errors.name?.message}</p>
       </div>
+
       <div>
-        
+        <label>Email</label>
+        <input {...register('email')} />
+        <p>{errors.email?.message}</p>
       </div>
-  </>
+
+      <div>
+        <label>Your weight in kilograms:</label>
+        <input type="number" {...register('weight')} />
+        <p>{errors.weight?.message}</p>
+      </div>
+
+      <div>
+        <label>The time of active participation in sports:</label>
+        <input type="number" {...register('sportTime')} />
+        <p>{errors.sportTime?.message}</p>
+      </div>
+
+      <div>
+        <label>The required amount of water in liters per day:</label>
+        <input type="number" {...register('waterAmount')} />
+        <p>{errors.waterAmount?.message}</p>
+      </div>
+
+      <input type="submit" />
+    </form>
   );
 }
+
+// import { useState } from 'react';
+// const favouriteBooks = [
+//   { id: "id-1", name: "JS for beginners" },
+//   { id: "id-2", name: "React basics" },
+//   { id: "id-3", name: "React Router overview" }
+// ];
+
+// import productList from './productList.json'
+// import cats from './cats.json'
+
+// import BestProduct from "./BestProduct/BestProduct";
+// import BookList from "./BookList";
+// import Cats from './Cats/Cats';
+// import CatsListBtn from './CatsListBtn/CatsListBtn';
+
+// const initialLikes = { cat1: 0, cat2: 0, cat3: 0, cat4: 0 };
+
+// export default function App() {
+//   const [likes, setLikes] = useState(initialLikes );
+
+//   const handleLogLikes = (catName) => {
+//   setLikes({ ...likes, [catName]: likes[catName] + 1 })
+//   };
+//   const handleResetLikes = () => { setLikes(initialLikes); };
+//  const totalLike = Object.values(likes).reduce((acc, item) => acc+item, 0)
+
+//   return (
+//     <>
+//       <div>
+//         <h1>Send Likes for cats</h1>
+//         <CatsListBtn likes={likes}
+//           handleLogLikes={handleLogLikes}
+//           handleResetLikes={handleResetLikes}
+//        totalLike={totalLike}
+//         />
+//       </div>
+//       <div>
+//         <h1>Best cats</h1>
+//         <Cats cats={cats} />
+//       </div>
+
+//      <div>
+// 			<h1>Books of the week you have to read</h1>
+//       <BookList books={favouriteBooks} />
+//       </div>
+
+//       <div>
+//         <h1>Best products</h1>
+//         <BestProduct productList={ productList} />
+//       </div>
+//       <div>
+
+//       </div>
+//   </>
+//   );
+// }
